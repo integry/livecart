3d92257bd2c78a5f369634c71e287a77ec98c7d3
sergej <devnull@localhost>
2006-10-25 15:11:34 +0300

    [svn]

--- application/controller/backend/CatalogController.php
+++ application/controller/backend/CatalogController.php
@@ -17,25 +17,225 @@ ClassLoader::import("application.model.locale.Language");
 class CatalogController extends StoreManagementController
 {
 
-	private $multi_language;
+	private $multi_language = 'en';
 	
 	/**
 	 */
 	public function init()
 	{
-		/*
 		parent::init();
 
-		$this->addBlock("NAV", "treeSection");
+//		$this->addBlock("NAV", "treeSection");
+		$this->setLayout("categoryManager");
 
 		@session_start();
-		$this->multi_language = isSet($_SESSION['CURRENT_MULTI_LANGUAGE']) ? $_SESSION['CURRENT_MULTI_LANGUAGE']: 'en';
 
 		//js sources
 		$app = Application::getInstance();
-		*/
 	}
 
+	/**
+	 * Index
+	 */
+	public function index()
+	{
+		ClassLoader::import("framework.request.validator.Form");
+				
+		$response = new ActionResponse();
+		$response->setValue("catalogForm", $this->createCatalogForm());
+		return $response;
+	}
+	
+	/**
+	 * Add catalog form
+	 */
+	public function add()
+	{		
+		$response = new ActionResponse();
+		$response->setValue("catalogForm", $this->createCatalogForm());
+		
+		return $response;
+	}
+	
+	/**
+	 * Update catalog form
+	 */
+	public function update()
+	{
+		if($id = $this->request->getValue('id', false))
+		{			
+			$response = new ActionResponse();
+			$response->setValue("catalogForm", $this->createCatalogForm());
+			$response->setValue('id', $id);
+			
+			return $response;
+		}
+		else 
+		{
+			return new ActionRedirectResponse($this->request->getControllerName(), "index");
+		}
+	}		
+
+	/**
+	 * Saves main details of current catalog
+	 * 
+	 * @return ActionRedirectResponse
+	 */
+	public function save()
+	{
+		if ($id = $this->request->getValue("id", false))
+		{
+			return $this->updateCatalog($id);
+		} 
+		else 
+		{
+			return $this->createNewCatalog();
+		}
+	}
+
+	/**
+	 * Deletes catalog group.
+	 * 
+	 * @return ActionRecirectResponse
+	 */
+	public function delete()
+	{
+		if($id = $this->request->getValue('id', false))
+		{
+			TreeCatalog::delete($id);
+		}
+		
+		return new ActionRedirectResponse($this->request->getControllerName(), "index");
+	}
+	
+	/**
+	 * Moves category to another parent
+	 * 
+	 * @return ActionRedirectResponse
+	 */
+	public function move()
+	{
+		Tree::modifyTreeParent("Catalog", $this->request->getValue("id"), $this->request->getValue("moveto"));
+		return new ActionRedirectResponse($this->request->getControllerName(), "index", array("id" => $this->request->getValue("moveto")));
+	}
+		
+	/**
+	 * Form of movieng node to another.
+	 * @return ActionResponse
+	 */
+	public function moveForm()
+	{
+		if ($id = $this->request->getValue("id", false))
+		{
+			// resposne
+			// $response->setValue("group", $this->createTreeHTML("formatTreeMenuForMove", "objTreeMenuAjax_catalogMove", "objTreeMenuAjax_catalogMove")->toHtml("objTreeMenuAjax_catalogMove"));
+
+			$response = new ActionResponse();
+			$response->setValue("id", $this->request->getValue("id"));
+			
+			return $response;
+		}
+		else 
+		{
+			die();
+		}
+	}	
+	
+	
+	
+	
+	/**
+	 * Save catalog (create new one)
+	 * 
+	 * @return ActionRedirectResponce
+	 */
+	private function createNewCatalog()
+	{
+		// Get catalog parrent or false (then root catalog will be used as a parent)
+		$parent = $this->request->getValue('parent', false);
+		
+		if ($this->createCatalogForm()->getValidator()->isValid())
+		{
+			$catalog = TreeCatalog::getNewTreeInstance($parent);
+
+			$catalog->lang($this->multi_language)->name->Set($this->request->getValue("name"));
+			$catalog->lang($this->multi_language)->description->set($this->request->getValue("description"));
+			
+			$catalog->Save();
+			
+			return new ActionRedirectResponse($this->request->getControllerName(), "index");
+		}
+		else
+		{			
+			return new ActionRedirectResponse($this->request->getControllerName(), "add", array("parent" => $parent));
+		}
+	}
+	
+	/**
+	 * Save catalog (update an existing catalog)
+	 * 
+	 * @return ActionRedirectResponce
+	 */
+	private function updateCatalog($id)
+	{		
+		if ($this->createCatalogForm()->getValidator()->isValid())
+		{
+			$catalog = Catalog::getInstanceById($id);
+			
+			$catalog->lang($this->multi_language)->name->set($this->request->getValue("name"));
+			$catalog->lang($this->multi_language)->description->set($this->request->getValue("description"));
+			
+			$catalog->save();
+			
+			return new ActionRedirectResponse($this->request->getControllerName(), "index");
+		}
+		else
+		{
+			return new ActionRedirectResponse($this->request->getControllerName(), "update", array("id" => $id));
+		}
+
+	}
+	
+	/**
+	 * Creates form object and defines validation rules
+	 * 
+	 * @return Form
+	 */
+	private function createCatalogForm()
+	{
+		ClassLoader::import("framework.request.validator.*");
+		
+		$validator = new RequestValidator("catalogForm", $this->request);
+		$validator->addCheck("name", new MinLengthCheck($this->translate("Name must be at least two chars length"), 2));
+		
+		$form = new Form($validator);
+		
+		if ($this->request->isValueSet("id"))
+		{
+			$catalog = ActiveRecord::getInstanceById('CatalogLangData', array('catalogID' => $this->request->getValue("id"), 'languageID' => $this->multi_language), true);
+			$data = $catalog->toArray();
+				
+			$form->setData(array(
+				'name' => $data['name'],
+				'description' => 'description'
+			));
+		} 
+		else if($this->request->isValueSet('parent')) 
+		{
+			$form->setValue('parent', $this->request->getValue('parent'));
+		}
+	
+		return $form; 
+	}
+	
+	
+
+	
+	
+
+	
+
+	
 	/**
 	 * Block of group tree.
 	 */
@@ -43,35 +243,18 @@ class CatalogController extends StoreManagementController
 	{
 		//response
 		$response = new BlockResponse();
-		$response->setValue("title", $this->locale->translate("_catalogTree"));
+		$response->setValue("title", $this->translate("_catalogTree"));
 		$response->setValue("group", $this->createTreeHTML("formatTreeMenu", "objTreeMenuAjax_catalog", "objTreeMenuAjax_catalog")->toHtml());
 		$response->setValue("id", $this->request->getValue("id"));
 
 		//js sources
 		$app = Application::getInstance();
 		$app->getRenderer()->appendValue("JAVASCRIPT", Router::getInstance()->getBaseDir()."/public/javascript/document.js");
+		
 		//ajaxtreemenu js sources
 		$app->getRenderer()->appendValue("JAVASCRIPT", Router::getInstance()->getBaseDir()."/public/javascript/ajax.js");
 		$app->getRenderer()->appendValue("JAVASCRIPT", Router::getInstance()->getBaseDir()."/public/javascript/TreeMenuAjax.js");
 
-		return $response;
-	}
-
-	/**
-	 * Form of movieng node to another.
-	 * @return ActionResponse
-	 */
-	public function moveForm()
-	{
-		if (!$this->request->isValueSet("id"))
-		{
-			exit();
-		}
-
-		//resposne
-		$response = new ActionResponse();
-		$response->setValue("group", $this->createTreeHTML("formatTreeMenuForMove", "objTreeMenuAjax_catalogMove", "objTreeMenuAjax_catalogMove")->toHtml("objTreeMenuAjax_catalogMove"));
-		$response->setValue("id", $this->request->getValue("id"));
 		return $response;
 	}
 
@@ -83,15 +266,21 @@ class CatalogController extends StoreManagementController
 	 */
 	private function createTreeHTML($format_method, $js_object_name, $method)
 	{
-		$tree = TreeCatalog::getAllTree("TreeCatalog", true);
-
+//		ClassLoader::import("library.AJAX_TreeMenu.*");
+		
+//		$tree = TreeCatalog::getAllTree("TreeCatalog", true);
+		
 		//format TreeMenu
-		$treemenu = new AJAX_TreeMenu();
-		$this->$format_method($treemenu, $tree, $js_object_name, $method);
+//		$treemenu = new AJAX_TreeMenu();
+//		$this->$format_method($treemenu, $tree, $js_object_name, $method);
 
 		//html
-		return new AJAX_TreeMenu_DHTML("", $js_object_name, $treemenu, array('images' => Router::getInstance()->getBaseDir().'/library/AJAX_TreeMenu/imagesAlt2', 'defaultClass' => 'treeMenuDefault'));
+		
+//		return new RawResponse("OK");
+//		return new AJAX_TreeMenu_DHTML("", $js_object_name, $treemenu, array('images' => Router::getInstance()->getBaseDir().'/library/AJAX_TreeMenu/imagesAlt2', 'defaultClass' => 'treeMenuDefault'));
 	}
+
+
 
 	/**
 	 * Formats AJAX_TreeMenu object $treemenu.
@@ -136,7 +325,7 @@ class CatalogController extends StoreManagementController
 	{
 		$array = array();
 		$array['text'] = $tree->hasId() ? $tree->lang("en")->name->get(): "ALL";
-		$array['link'] = Router::getInstance()->createUrl(array("controller" => "backend.catalog", "action" => "index", "id" => $tree->getId()));
+		$array['link'] = Router::getInstance()->createUrl(array("controller" => $this->request->getControllerName(), "action" => "index", "id" => $tree->getId()));
 		$array['cssClass'] = (!$tree->hasId() && !$this->request->isValueSet("id")) || ($tree->hasId() && $tree->getId() == $this->request->getValue("id")) ? 'treeMenuNodeSelected' : 'treeMenuNode';
 		return $array;
 	}
@@ -155,188 +344,12 @@ class CatalogController extends StoreManagementController
 		return $array;
 	}
 
-	/**
-	 * Moves category to another parent.
-	 * @return ActionRedirectResponse
-	 */
-	public function move()
-	{
-		Tree::modifyTreeParent("Catalog", $this->request->getValue("id"), $this->request->getValue("moveto"));
-		return new ActionRedirectResponse($this->request->getControllerName(), "index", array("id" => $this->request->getValue("moveto")));
-	}
 
-	/**
-	 * Main action.
-	 */
-	public function index()
-	{
-		$this->setLayout("categoryManager");
-		ClassLoader::import("framework.request.validator.Form");
-		
-		$form = new Form(new RequestValidator("catalog", $this->request));
-		$response = new ActionResponse();
-		$response->setValue("catalogForm", $form);
-		return $response;
-		
-		/*
-		if ($this->request->isValueSet("id"))
-		{
-			//languages
-			$langs = Language::getLanguages(1)->toArray();
 
-			//form handling
-			$form = $this->createCatalogForm(array());
-			$form->setAction(Router::getInstance()->createUrl(array('controller' => 'backend.catalog', 'action' => 'save', 'id' => $this->request->getValue("id"))));
 
-			if ($form->validationFailed())
-			{
-				$form->restore();
-			}
-			else
-			{
-				$catalog = Catalog::getInstanceById($this->request->getValue("id"), true);
-				$data = $catalog->toArray();
-				$form->setData($data['lang'][$this->multi_language]);
-			}
 
-			$response = new ActionResponse();
-			$response->setValue("action", "index");
-			$response->setValue("current_lang", $this->multi_language);
-			$response->setValue("langs", $langs);
-			$response->setValue("id", $this->request->getValue("id"));
 
-			if ($this->multi_language != "en")
-			{
-				$response->setValue("english_name", $data['lang']['en']['name']);
-				$response->setValue("english_description", $data['lang']['en']['description']);
-			}
 
-			$response->setValue("file_content", "backend/catalog/index.content.tpl");
-			//rendering "backend/catalog/index.content.tpl"
-			$response->setValue("content", "<br>".@$form->render());
-
-			return $response;
-		}
-		else
-		{
-			return new ActionResponse();
-		}
-		*/
-	}
-
-	/**
-	 * Subgroup adding form.
-	 * @return ActionResponse
-	 */
-	public function addForm()
-	{
-		//form
-		$form = $this->createCatalogForm(array());
-		$form->setAction(Router::getInstance()->createUrl(array('controller' => 'backend.catalog', 'action' => 'add', 'id' => $this->request->getValue("id"))));
-
-		if ($form->validationFailed())
-		{
-			$form->restore();
-		}
-
-		//response
-		$response = new ActionResponse();
-		$response->setValue("form", @$form->render());
-		return $response;
-	}
-
-	/**
-	 *
-	 * @param array $data Initial values
-	 * @return Form
-	 * @todo For saving part for displaying form is not neeted go //See 1
-	 */
-	public function createCatalogForm($data)
-	{
-		ClassLoader::import("library.formhandler.*");
-		ClassLoader::import("library.formhandler.check.string.*");
-
-		$form = new Form("catalogForm", $data);
-
-		$field = new TextLineField("name", $this->locale->translate("_name"));
-		$field->addCheck(new MinLengthCheck($this->locale->translate("_nameMustBeAtLeast2CharsLength"), 2));
-		$form->addField($field);
-
-		$field = new TextareaField("description", $this->locale->translate("_description"));
-		;
-		$form->addField($field);
-
-		$form->addField(new SubmitField("submit", $this->locale->translate("_save")));
-
-		// This is done just for displaying form //See 1
-		$form->getField("name")->setAttribute("maxlength", 100);
-
-		return $form;
-	}
-
-	/**
-	 * Saves main details of current catalog
-	 * @return ActionRedirectResponse
-	 */
-	public function save()
-	{
-		if ($this->request->isValueSet("id"))
-		{
-			$form = $this->createCatalogForm($this->request->toArray());
-			if ($form->isValid())
-			{
-				$catalog = Catalog::getInstanceById($this->request->getValue("id"));
-				$catalog->lang($this->multi_language)->name->set($this->request->getValue("name"));
-				$catalog->lang($this->multi_language)->description->set($this->request->getValue("description"));
-				$catalog->save();
-			}
-			else
-			{
-				$form->saveState();
-			}
-
-			return new ActionRedirectResponse("backend.catalog", "index", array("id" => $this->request->getValue("id")));
-		}
-	}
-
-	/**
-	 * Adds catalog subgroup.
-	 * @return ActionRedirectResponse
-	 */
-	public function add()
-	{
-		$form = $this->createCatalogForm($this->request->toArray());
-
-		if ($form->isValid())
-		{
-			$catalog = TreeCatalog::getNewTreeInstance($this->request->isValueSet("id") ? $this->request->getValue("id"): false);
-
-			$catalog->lang("en")->name->Set($this->request->getValue("name"));
-			$catalog->lang("en")->description->set($this->request->getValue("description"));
-			$catalog->Save();
-
-			return new ActionRedirectResponse("backend.catalog", "AddForm", array("id" => $this->request->getValue("id")));
-		}
-		else
-		{
-			//redirect response if form not valid
-			$form->saveState();
-			return new ActionRedirectResponse("backend.catalog", "AddForm", array("id" => $this->request->getValue("id")));
-		}
-	}
-
-	/**
-	 * Deletes catalog group.
-	 * @return ActionRecirectResponse
-	 */
-	public function delete()
-	{
-		if ($this->request->isValueSet("id"))
-		{
-			Tree::delete("Catalog", $this->request->getValue("id"));
-		}
-		return new ActionRedirectResponse($this->request->getControllerName(), "index");
-	}
 
 	/**
 	 * Changes current multi language.
@@ -347,7 +360,7 @@ class CatalogController extends StoreManagementController
 		@session_start();
 		$_SESSION['CURRENT_MULTI_LANGUAGE'] = $this->request->getValue("multi_language");
 
-		return new ActionRedirectResponse("backend.catalog", "index", array("id" => $this->request->getValue("id")));
+		return new ActionRedirectResponse($this->request->getControllerName(), "index", array("id" => $this->request->getValue("id")));
 	}
 
 	public function fields()
