cbffd5da72cd788a2d75c671266c9b4645a2eef8
saulius <devnull@localhost>
2006-10-30 16:45:31 +0200

    [svn]

--- public/javascript/backend/categoryManager.js
+++ public/javascript/backend/categoryManager.js
@@ -7,11 +7,36 @@ LiveCart.CategoryManager = {
 	
 	init: function() 
 	{
+		this.initCategoryBrowser();
+		this.initTabs();
+	},
+	
+	initCategoryBrowser: function()
+	{
 		var tree = new dhtmlXTreeObject("categoryBrowser","100%","100%", 0);
+		tabbar.setImagePath("/livecart/public/image/backend/dhtmlxtree/");
+		
 		tree.insertNewItem(0,1,"Some Root Node",0,0,0,0,"SELECT");
 		tree.insertNewItem(0,2,"New Node zero",0,0,0,0,"SELECT");
 		tree.insertNewItem(1,3,"New Node one",0,0,0,0,"SELECT");
 		tree.insertNewItem(1,4,"New Node two",0,0,0,0,"SELECT");
 		tree.insertNewNext(1,5,"New Node three",0,0,0,0,"SELECT");
+	},
+	
+	initTabs: function()
+	{
+		var tabbar = new dhtmlXTabBar("managerContainer","top");
+		tabbar.setImagePath("/livecart/public/image/backend/dhtmlxtabbar/");
+		
+		tabbar.addTab("mainContent","Main Details", "100px");
+		tabbar.addTab("fieldContent","Fields", "100px");
+		tabbar.addTab("filterContent","Filters", "100px");
+		tabbar.addTab("subcategoryOrderContent","Subcategory Order", "100px");
+		tabbar.addTab("permissionContent","Permissions", "100px");
+		tabbar.addTab("imageContent","Images", "100px");
+		tabbar.addTab("articleContent","Articles", "100px");
+
+		
+		tabbar.setContentHTML("mainContent", "this is content of main details");
 	}
 }
