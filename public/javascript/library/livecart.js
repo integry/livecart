678cf5e38a6f9e674f3163ef35cbd53037ae0be3
sergej <devnull@localhost>
2006-10-31 19:42:48 +0200

    [svn] Templaito kuris bus siunciamas su ajaxu pradzioj galyma nauduoti <head>
    Pvz.
    
    <head>
        <script type="text/javascript" src="javascript/library/prototype/prototype.js"></script>
        <script type="text/javascript" src="javascript/backend/specFieldManager.js"></script>
    </head>

--- public/javascript/library/livecart.js
+++ public/javascript/library/livecart.js
@@ -3,6 +3,81 @@ var LiveCart = {
 }
 
 LiveCart.AjaxUpdater = Class.create();
+
+/**
+ * Update HTML head with HTTPXMLRequest (static method, scary :D)
+ *
+ * You can use <head>...</head> in your templates to update page head nodes (such as title, script, link). It also
+ * Should be a first tag u used in your template. If script function sees that such node allready exists in head 
+ * then that node is removed and recreated with new attributes. 
+ *
+ * Just a note... Try <head><title>Example</title></head>... *modified* it won't work :}
+ *
+ * @todo When new node is loaded there is no way to completely remove it from head until you refresh the page
+ * @todo Test on all browsers (now it is tested only with Firefox)
+ * @todo Refresh window title (Meaby there are more things to refresh)
+ *
+ * @param responce object
+ */
+LiveCart.AjaxUpdater.updateHead = function(response)
+{
+	var headTag = "</head>";
+	
+	if(response.responseText.indexOf(headTag) === -1) return;
+	
+	var headTagEnd = response.responseText.indexOf(headTag) + headTag.length;
+	var newHeadElements = (new DOMParser()).parseFromString(response.responseText.substring(0, headTagEnd), "text/xml").getElementsByTagName("head")[0].getElementsByTagName("*");
+	
+	var oldHeadElements = document.getElementsByTagName("head")[0];
+
+	
+	for(var i = 0; i < newHeadElements.length; i++) 
+	{
+		var similarElements = oldHeadElements.getElementsByTagName(newHeadElements[i].tagName);
+		var element = document.createElement(newHeadElements[i].tagName);
+		
+		var elementAttributes = newHeadElements[i].attributes;
+		var attributesLength = newHeadElements[i].attributes.length;
+		for(var j = 0; j < attributesLength; j++)
+		{
+			var attr = newHeadElements[i].attributes[j].nodeName;
+			var value = newHeadElements[i].attributes[j].nodeValue;
+			element[attr] = value;
+		}
+		
+		if(newHeadElements[i].firstChild)
+		{
+			element.appendChild(document.createTextNode(newHeadElements[i].firstChild.nodeValue));
+		}
+		
+		for(var j = 0; j < similarElements.length; j++)
+		{
+			// Delete old element
+			switch(element.tagName)
+			{
+				case 'script':
+					if(similarElements[j].src = element.src) 
+					{							
+						Element.remove(similarElements[j]);
+					}
+				break;
+				case 'link':
+					if(similarElements[j].href = element.href) 
+					{
+						Element.remove(similarElements[j]);
+					}
+				break;
+				default:
+					Element.remove(similarElements[j]);
+				break;
+			}
+			
+			// Insert new tag into head
+			oldHeadElements.appendChild(element);
+		}
+	}
+}
+
 LiveCart.AjaxUpdater.prototype = {
 	
 	indicatorContainerId: null,
@@ -66,10 +141,15 @@ LiveCart.AjaxUpdater.prototype = {
 						 updaterOptions);
 	},
 	
+	
 	hideIndicator: function(response)
 	{
+		// It would better if i could use something like "this" here
+		LiveCart.AjaxUpdater.updateHead(response);
+		
 		Element.hide(LiveCart.ajaxUpdaterInstance.indicatorContainerId);
 	},
+	
 	
 	reportError: function(response) 
 	{
