da3a40a695f8ed2bea0d220a64589511fba93920
rinalds <devnull@localhost>
2006-11-07 16:54:37 +0200

    [svn]

---
+++ application/helper/function.loadScriptaculous.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Load JS files needed to initialize Scriptaculous
+ *
+ * Usage example:
+ * <code>
+ *	{loadScriptaculous}
+ * </code>
+ *
+ * @param array $params
+ * @param Smarty $smarty
+ * @return string
+ * 
+ * @package application.helper
+ * @author Rinalds Uzkalns <rinalds@integry.net>
+ */
+function smarty_function_loadScriptaculous($params, $smarty) 
+{
+	require_once('function.includeJs.php');
+	smarty_function_includeJs(array('file' => 'library/prototype/prototype.js'), $smarty);
+	smarty_function_includeJs(array('file' => 'library/scriptaculous/scriptaculous.js'), $smarty);
+}
+
+?>
