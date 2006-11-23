305852b4f57f58b3f0c42abae35ba11157f44fd5
sergej <devnull@localhost>
2006-11-23 18:20:42 +0200

    [svn] ""

--- public/javascript/backend/specFieldManager.js
+++ public/javascript/backend/specFieldManager.js
@@ -1,3 +1,46 @@
+/**
+ * LiveCart.SpecFieldManager
+ *
+ * Script for managing spec field form
+ *
+ * The following class manages spec field forms. I have used an separate js file (a class)
+ * because there are a lot of thing happening when you are dealing with spec fields forms.
+ *
+ * To use this class you should simply pass specFIelds values to it like so
+ * @example
+ * <code>
+ *     new LiveCart.SpecFieldManager({
+ *        "ID":"new",
+ *        "name":"a:2:{s:2:\"en\";s:11:\"Electronics\";s:2:\"lt\";s:11:\"Elektronika\";}",
+ *        "description":[],
+ *        "handle":"",
+ *        "values":[],
+ *        "rootId": "specField_item_new",
+ *        "type":5,
+ *        "dataType":2
+ *     });
+ * </code>
+ *
+ * I hope whoever reads this will figure aut what each value means. Name, description and values
+ * can have multiple values for each language
+ *
+ * Also you should know that some values are not meant to be passed to constructor (it will also
+ * work fine... meaby) Here is an example
+ *
+ * @example
+ * <code>
+ *     LiveCart.SpecFieldManager.prototype.languages = {"en":"English","lt":"Lithuanian","de":"German"};
+ *     LiveCart.SpecFieldManager.prototype.types = createTypesOptions({"2":{"1":"Selector","2":"Numbers"},"1":{"3":"Text","4":"Word processer","5":"selector","6":"Date"}});
+ *     LiveCart.SpecFieldManager.prototype.messages = {"deleteField":"delete field"};
+ *     LiveCart.SpecFieldManager.prototype.selectorValueTypes = [1,5];
+ *     LiveCart.SpecFieldManager.prototype.doNotTranslateTheseValueTypes = [2];
+ *     LiveCart.SpecFieldManager.prototype.countNewValues = 0;
+ * </code>
+ *
+ * @version 1.0
+ * @author Sergej Andrejev
+ */
+
 if (LiveCart == undefined)
 {
 	var LiveCart = {}
@@ -8,7 +51,10 @@ LiveCart.SpecFieldManager.prototype = {
 	/**
 	 * Constructor
 	 *
-	 * @var types Hash of options (where hash key is value type and value is array of Option objects)
+	 * @var specFields Spec Field values
+	 *
+	 * @access public
+	 *
 	 */
 	initialize: function(specField)
 	{
@@ -25,6 +71,8 @@ LiveCart.SpecFieldManager.prototype = {
 		this.dataType = specField.dataType;
 		this.translations = specField.translations;
 
+		this.isNew = specField.isNew;
+
 		this.findUsedNodes();
 		this.bindFields();
 	},
@@ -32,6 +80,8 @@ LiveCart.SpecFieldManager.prototype = {
 
 	/**
 	 * Find ussed nodes
+	 *
+	 * @access private
 	 *
 	 */
 	findUsedNodes: function()
@@ -47,6 +97,13 @@ LiveCart.SpecFieldManager.prototype = {
 		this.nodes.stepMain 			= document.getElementsByClassName("step_main", this.nodes.parent)[0];
 
 		this.nodes.stepLevOne 			= document.getElementsByClassName("step_lev1", this.nodes.parent);
+
+		for(var i = 0; i < this.nodes.stepLevOne.length; i++)
+		{
+            var test = this.nodes.stepLevOne[i];
+		    if(!this.nodes.stepLevOne[i].id) this.nodes.stepLevOne[i].id = this.nodes.stepLevOne[i].className.replace(/ /, "_") + "_" + this.id;
+		}
+
 		this.nodes.mainTitle 			= document.getElementsByClassName("specField_title", this.nodes.parent)[0];
 		this.nodes.id 					= document.getElementsByClassName("specField_form_id", this.nodes.parent)[0];
 		this.nodes.description 			= document.getElementsByClassName("specField_form_description", this.nodes.parent)[0];
@@ -58,6 +115,12 @@ LiveCart.SpecFieldManager.prototype = {
 		this.nodes.valuesAddFieldLink 	= this.nodes.valuesDefaultGroup.getElementsByClassName("add_field", this.nodes.parent)[0];
 	},
 
+	/**
+	 * Find all translations fields. This is done every time when new field is being added
+	 *
+	 * @access private
+	 *
+	 */
 	bindTranslationValues: function()
 	{
 		this.nodes.translatedValues = document.getElementsByClassName("specField_form_values_translations", this.nodes.parent);
@@ -67,6 +130,8 @@ LiveCart.SpecFieldManager.prototype = {
 
 	/**
 	 * Binds fields to some events
+	 *
+	 * @access private
 	 *
 	 */
 	bindFields: function()
@@ -96,6 +161,13 @@ LiveCart.SpecFieldManager.prototype = {
 		this.typeWasChangedAction();
 	},
 
+	/**
+	 * Whem Mike changes input type from "numbers" to "text" programm should select
+	 * appropriate value from types list (like selector, text, date, number, etc)
+	 *
+	 * @access private
+	 *
+	 */
 	loadTypes: function()
 	{
 		if(this.type)
@@ -111,6 +183,13 @@ LiveCart.SpecFieldManager.prototype = {
 		}
 	},
 
+	/**
+	 * When the value type changes whe should decide whether show step "Values" (for selectors) or not,
+	 * and whether to show translations or not (show for text, hide for numbers)
+	 *
+	 * @access private
+	 *
+	 */
 	typeWasChangedAction: function()
 	{
 		// if selected type is a selector type then show selector options fields (aka step 2)
@@ -133,49 +212,59 @@ LiveCart.SpecFieldManager.prototype = {
 	},
 
 
+	/**
+	 * This method binds all default values (those which are field in "Values" step) and create new fields in "Translations"
+	 * step where user can fill translations for those values
+	 *
+	 * @access private
+	 *
+	 */
 	bindDefaultFields: function()
 	{
 		var self = this;
 		$A(this.nodes.valuesDefaultGroup.getElementsByTagName("input")).each(function(input)
 		{
 			input.onkeyup = self.mainValueFieldChangedAction.bind(self);
+			input.onkeydown = self.mainValueFieldChangedAction.bind(self);
 		});
 
-		try
-		{
-    		require_once('backend/activeList.js');
-    	    new LiveCart.ActiveList(this.nodes.valuesDefaultGroup.getElementsByTagName("ul")[0], {
-    	        beforeSort: function(li, order){ return 'sort.php?'+order},
-    	        afterSort: function(li, response){ },
-    	        beforeEdit: function(li){ },
-    	        afterEdit: function(li, response){ },
-    	        beforeDelete: function(li){ if(confirm('Are you realy want to delete this item?')) return 'delete.php?id='+this.getRecordId(); },
-    	        afterDelete: function(li, response){ Element.remove(li) }
-    	    });
-		}
-		catch(e)
-		{
-		    jsTrace.debug(e)
-		}
+		require_once('backend/activeList.js');
+	    new LiveCart.ActiveList(this.nodes.valuesDefaultGroup.getElementsByTagName("ul")[0], {
+	        beforeSort: function(li, order){ return 'sort.php?'+order},
+	        afterSort: function(li, response){ },
+	        beforeEdit: function(li){ },
+	        afterEdit: function(li, response){ },
+	        beforeDelete: function(li){ if(confirm('Are you realy want to delete this item?')) return 'delete.php?id='+this.getRecordId(); },
+	        afterDelete: function(li, response){ self.deleteValueFieldAction(li) }
+	    });
 	},
 
 
+	/**
+	 * Here we fill "Main" step field values like name, handle, input type and value type
+	 *
+	 * @access private
+	 *
+	 */
 	loadSpecFieldAction: function()
 	{
         var self = this;
 
 	    // Default language
-		this.nodes.id.value = this.id;
-		this.nodes.handle.value = this.handle;
+		if(this.id) this.nodes.id.value = this.id;
+		if(this.handle) this.nodes.handle.value = this.handle;
 
-		this.nodes.title.value = this.name[this.languageCodes[0]];
+		if(this.name[this.languageCodes[0]]) this.nodes.title.value = this.name[this.languageCodes[0]];
 		this.nodes.title.name = "name[" + this.languageCodes[0] + "]";
 
 		this.nodes.multipleSelector.checked = this.multipleSelector ? true : false;
 
-		this.nodes.mainTitle.firstChild.nodeValue = this.nodes.title.value;
+		if(this.nodes.mainTitle)
+		{
+    		this.nodes.mainTitle.firstChild.nodeValue = this.nodes.title.value;
+		}
 
-		this.nodes.description.value = this.description[this.languageCodes[0]];
+		if(this.description[this.languageCodes[0]]) this.nodes.description.value = this.description[this.languageCodes[0]];
 		this.nodes.description.name = "description[" + this.languageCodes[0] + "]";
 
 		// select dataType (or first)
@@ -223,8 +312,7 @@ LiveCart.SpecFieldManager.prototype = {
 				{
                     if(Element.hasClassName(inputFields[j].parentNode, 'step_translations_language'))
                     {
-    				    jsTrace.send("inputFields[j].value = self."+inputFields[j].name+"['"+self.languageCodes[i]+"'];");
-    				    eval("inputFields[j].value = self."+inputFields[j].name+"['"+self.languageCodes[i]+"'];");
+    				    eval("if(self."+inputFields[j].name+"['"+self.languageCodes[i]+"']) [j].value = self."+inputFields[j].name+"['"+self.languageCodes[i]+"'];");
     					inputFields[j].name = inputFields[j].name + "[" + self.languageCodes[i] + "]";
                     }
 				}
@@ -243,7 +331,10 @@ LiveCart.SpecFieldManager.prototype = {
 
 
 	/**
-	 * Load values when page is loaded
+	 * When we create form from JSON string we should create and fill in values fields (from "Values" step)
+	 * and their translations in "Translations" step if needed
+	 *
+	 * @access private
 	 *
 	 */
 	loadValueFieldsAction: function()
@@ -260,7 +351,11 @@ LiveCart.SpecFieldManager.prototype = {
 
 
 	/**
-	 * Load languages
+	 * This method separates language codes from language titles
+	 *
+	 * @example (lt: Lithuanian, ru: Russian) will create [lt, ru] array
+	 *
+	 * @access private
 	 *
 	 */
 	loadLanguagesAction: function()
@@ -273,6 +368,14 @@ LiveCart.SpecFieldManager.prototype = {
 		});
 	},
 
+	/**
+	 * In SpecField form template we not yet know what languages we'll be using so
+	 * what we are doing here is looking at what languages we are using and creating separate
+	 * sections for each language in "Translations" section
+	 *
+	 * @access private
+	 *
+	 */
 	createLanguagesLinks: function()
 	{
 		var languageTemplateLink = document.getElementsByClassName("dom_template", this.nodes.translationsLinks)[0];
@@ -294,6 +397,15 @@ LiveCart.SpecFieldManager.prototype = {
 		}
 	},
 
+	/**
+	 * Programm should change language section if we have click on a link meaning different language. If we click current
+	 * language it will callapse (not the programme of course =)
+	 *
+	 * @var Event e Event
+	 *
+	 * @access private
+	 *
+	 */
 	changeTranslationLanguageAction: function(e)
 	{
 		if(!e)
@@ -314,7 +426,14 @@ LiveCart.SpecFieldManager.prototype = {
 	},
 
 	/**
-	 * Add new field to values
+	 * When we add new value "Values" step we are also adding it to "Translations" step. Field name
+	 * will have new_3 (or any other number) in its name. We are not realy creating a field here. Instead
+	 * we are calling for addField method to do the job. The only usefull thing we are doing here is
+	 * generating an id for new field
+	 *
+	 * @var Event e Event
+	 *
+	 * @access private
 	 *
 	 */
 	addValueFieldAction: function(e)
@@ -333,41 +452,35 @@ LiveCart.SpecFieldManager.prototype = {
 
 
 	/**
-	 * Delete field
+	 * This one is easy. When we click on delete value from "Values" step we delete the value and it's
+	 * translation in "Translations" step
+	 *
+	 * @var Event e Event
+	 *
+	 * @access private
 	 *
 	 */
-	deleteValueFieldAction: function(e)
+	deleteValueFieldAction: function(li)
 	{
-		if(confirm(this.messages.deleteField))
+		var splitedHref = li.id.split("_");
+		var isNew = splitedHref[splitedHref.length - 2] == 'new' ? true : false;
+		var id = (isNew ? 'new_' : '') + splitedHref[splitedHref.length - 1];
+
+		for(var i = 0; i < this.languageCodes.length; i++)
 		{
-			if(!e)
+			var translatedValue = document.getElementById("specField_form_values_" + this.languageCodes[i] + "_" + id);
+
+			// if new or not main language
+			if(isNew || i > 0)
 			{
-				e = window.event;
-				e.target = e.srcElement;
+				Element.remove(translatedValue);
 			}
-
-			Event.stop(e);
-
-			var splitedHref = e.target.parentNode.id.split("_");
-			var isNew = splitedHref[splitedHref.length - 2] == 'new' ? true : false;
-			var id = (isNew ? 'new_' : '') + splitedHref[splitedHref.length - 1];
-
-			for(var i = 0; i < this.languageCodes.length; i++)
+			else
 			{
-				var translatedValue = document.getElementById("specField_form_values_" + this.languageCodes[i] + "_" + id);
-
-				// if new or not main language
-				if(isNew || i > 0)
-    			{
-    				Element.remove(translatedValue);
-    			}
-    			else
-    			{
-    			    translatedValue.id += '_deleted';
-    			    var input = translatedValue.getElementsByTagName('input')[0];
-    			    input.name = input.name.replace(/\[\w+\]\[([\d]+)\]/, "[deleted][$1]");
-    			    translatedValue.style.display = 'none';
-    			}
+			    translatedValue.id += '_deleted';
+			    var input = translatedValue.getElementsByTagName('input')[0];
+			    input.name = input.name.replace(/\[\w+\]\[([\d]+)\]/, "[deleted][$1]");
+			    translatedValue.style.display = 'none';
 			}
 		}
 	},
@@ -375,6 +488,10 @@ LiveCart.SpecFieldManager.prototype = {
 
 	/**
 	 * This callback is executed when user change the value type
+	 *
+	 * @var Event e Event
+	 *
+	 * @access private
 	 *
 	 */
 	dataTypeChangedAction: function(e)
@@ -400,7 +517,12 @@ LiveCart.SpecFieldManager.prototype = {
 
 	/**
 	 * This callback is executed when user changes the state. When user change the state all other
-	 * states are hidden and only current state s shown
+	 * states are hidden and only current state shown or if the user was so stupid to click on current
+	 * state whe whole thing will crash (or the current step will collapse. I don't realy remember)
+	 *
+	 * @var Event e Event
+	 *
+	 * @access private
 	 *
 	 */
 	changeStateAction: function(e)
@@ -416,11 +538,45 @@ LiveCart.SpecFieldManager.prototype = {
 		var currentStep = e.target.hash.substring(1);
 		for(var i = 0; i < this.nodes.stepLevOne.length; i++)
 		{
-			this.nodes.stepLevOne[i].style.display = (this.nodes.stepLevOne[i].className.split(' ').indexOf(currentStep) === -1 || this.nodes.stepLevOne[i].style.display == 'block') ? 'none' : 'block';
+			this.nodes.stateLinks[i].id = 'change_state' + this.id;
+
+			if(this.nodes.stepLevOne[i].className.split(' ').indexOf(currentStep) === -1 || this.nodes.stepLevOne[i].style.display == 'block')
+			{
+			    this.nodes.stepLevOne[i].style.display = 'none';
+			}
+			else
+			{
+			    this.nodes.stepLevOne[i].style.display = 'block';
+			}
 		}
 	},
 
 
+	/**
+	 * When some dumbass creates/modifies value in "Values" step, we are automatically creating
+	 * a label for similar field in every language section in "Translations" step.
+	 *
+	 * @example If we tipe one in "Values" step like so
+	 * ___________
+	 * |One       |
+	 * ------------
+	 *
+	 * the programm will change label of similar fields in every translation language like so
+	 *
+	 * Lithuanian:
+	 *        ___________
+	 * One:   |Vienas    |
+	 *        ------------
+	 *
+	 * German:
+	 *        ___________
+	 * One:   |Einz      |   * I don't realy know how to write one in germat and also tooday i am to lazy to google for it :(
+	 *        ------------
+	 *
+	 * @var Event e Event
+	 *
+	 * @access private
+	 */
 	mainValueFieldChangedAction: function(e)
 	{
 		if(!e)
@@ -441,9 +597,39 @@ LiveCart.SpecFieldManager.prototype = {
 		}
 	},
 
+	mainValueFieldChangedAction: function(e)
+	{
+		if(!e)
+		{
+			e = window.event;
+			e.target = e.srcElement;
+		}
+
+		keyboard = new KeyboardEvent(e);
+
+		if(!(
+		    // you can use +/- as the first character
+    		(keyboard.getCursorPosition() == 0 && !e.target.value.match('[\-\+]') && (keyboard.getKey() == 109 || keyboard.getKey() == 107 || (keyboard.isShift() && keyboard.getKey() == 61))) ||
+    		// You even can use dots or commas, but only once and not as the first symbol
+    		(e.target.value != '' && !e.target.value.match('[\.\,]') && [110, 188, 190].indexOf(keyboard.getKey()) >= 0) ||
+    		// at last but not the least i have implemanted such a great feature, that you can use digits to create numbers. [applause]
+    		([48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105].indexOf(keyboard.getKey()) > 0) ||
+    		// special chars
+    		([46, 8, 17, 16, 37, 38, 39, 40].indexOf(keyboard.getKey()) >= 0)
+		))
+		{
+		    Event.stop(e);
+		}
+	},
 
 	/**
-	 * Automatically generates field name from title
+	 * When we are filling spec field name in "Main" step we are changing it's handle and a title
+	 * on the top of the form. Handle is actuali a stripped version of spec field name with all spec
+	 * symbols changed to "_" (underscope)
+	 *
+	 * @var Event e Event
+	 *
+	 * @access private
 	 *
 	 */
 	generateHandleAndTitleAction: function(e)
@@ -459,12 +645,17 @@ LiveCart.SpecFieldManager.prototype = {
 
 		this.nodes.handle.value = handle;
 
-		this.nodes.mainTitle.firstChild.nodeValue = this.nodes.title.value;
+		if(this.nodes.mainTitle) this.nodes.mainTitle.firstChild.nodeValue = this.nodes.title.value;
 	},
 
 
 	/**
-	 * Add new value field
+	 * Here we are adding new field to values list in "Values" step and "Translations" step.
+	 *
+	 * @var hash value Value of newly created field. The value is a hash array with value for every language {'en': "One", 'lt': "Vienas", 'de': "Einz"}
+	 * @var int id Id of a newly created field
+	 *
+	 * @access private
 	 *
 	 */
 	addField: function(value, id)
@@ -482,6 +673,7 @@ LiveCart.SpecFieldManager.prototype = {
 			var input = newValue.getElementsByTagName("input")[0];
 			input.name = "values[" + this.languageCodes[0] + "]" + (id ? "["+id+"]" : '[new][]');
 			input.value = (value && value[this.languageCodes[0]]) ? value[this.languageCodes[0]] : '' ;
+
 
 			var ul = this.nodes.valuesDefaultGroup.getElementsByTagName('ul')[0];
 			ul.id = 'specField_form_'+this.id+'_values_'+this.languageCodes[0];
