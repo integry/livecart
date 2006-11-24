f63bd29ccd0b6302c3d3bbb51eb523647451fe2a
sergej <devnull@localhost>
2006-11-24 17:11:14 +0200

    [svn] "public function toArray() -> public function toArray($convertToUnderscope=true)"

--- application/model/category/SpecFieldValue.php
+++ application/model/category/SpecFieldValue.php
@@ -15,10 +15,11 @@ class SpecFieldValue extends MultilingualObject
 		$schema = self::getSchemaInstance($className);
 		$schema->setName("SpecFieldValue");
 
+		$schema->registerField(new ARPrimaryKeyField("ID", ARInteger::instance()));
 		$schema->registerField(new ARForeignKeyField("specFieldID", "SpecField", "ID", "SpecField", ARInteger::instance()));
 
 		$schema->registerField(new ARField("value", ARArray::instance()));
-		$schema->registerField(new ARPrimaryKeyField("ID", ARInteger::instance()));
+//		$schema->registerField(new ARField("position", ARInteger::instance()));
 
 	}
 
@@ -30,6 +31,49 @@ class SpecFieldValue extends MultilingualObject
         return parent::getRecordSetArray(__CLASS__, $filter, false);
 	}
 
+	/**
+	 * Get blank active record instance
+	 *
+	 * @param unknown_type $recordID
+	 * @param unknown_type $loadRecordData
+	 * @param unknown_type $loadReferencedRecords
+	 * @return ActiveRecord
+	 */
+	public static function getNewInstance()
+	{
+	    return parent::getNewInstance(__CLASS__);
+	}
+
+	/**
+	 * Get cctive record instance
+	 *
+	 * @param unknown_type $recordID
+	 * @param unknown_type $loadRecordData
+	 * @param unknown_type $loadReferencedRecords
+	 * @return ActiveRecord
+	 */
+	public static function getInstanceByID($recordID, $loadRecordData = false, $loadReferencedRecords = false)
+	{
+	    return parent::getInstanceByID(__CLASS__, $recordID, $loadRecordData, $loadReferencedRecords);
+	}
+
+    /**
+     * Set a whole language field at a time. You can allways skip some language, bat as long as it occurs in
+     * languages array it will be writen into the database as empty string. I spent 2 hours writing this feature =]
+     *
+     * @example $specField->setLanguageField('name', array('en' => 'Name', 'lt' => 'Vardas', 'de' => 'Name'), array('lt', 'en', 'de'))
+     *
+     * @param string $fieldName Field name in database schema
+     * @param array $fieldValue Field value in different languages
+     * @param array $langCodeArray Language codes
+     */
+	public function setLanguageField($fieldName, $fieldValue, $langCodeArray)
+	{
+	    foreach ($langCodeArray as $lang)
+	    {
+	        $this->setValueByLang($fieldName, $lang, isset($fieldValue[$lang]) ? $fieldValue[$lang] : '');
+	    }
+	}
 }
 
 ?>
