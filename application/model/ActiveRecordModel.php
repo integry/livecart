847c950dc4ca1391966ace4e0bc7e3a83817f689
saulius <devnull@localhost>
2007-01-02 12:59:34 +0200

    [svn]

--- application/model/ActiveRecordModel.php
+++ application/model/ActiveRecordModel.php
@@ -6,16 +6,16 @@ ClassLoader::import("application.model.locale.*");
 
 ActiveRecord::$creolePath = ClassLoader::getRealPath("library.creole");
 
-ActiveRecord::setDSN("mysql://root@127.0.0.1/livecart_dev");
+ActiveRecord::setDSN("mysql://root@192.168.1.6/livecart_dev");
 ActiveRecord::getLogger()->setLogFileName(ClassLoader::getRealPath("cache") . DIRECTORY_SEPARATOR . "activerecord.log");
 
 /**
- * Base class for all ActiveRecord based models of application (single entry point in 
+ * Base class for all ActiveRecord based models of application (single entry point in
  * application specific model class hierarchy)
  *
  * @package application.model
  */
-abstract class ActiveRecordModel extends ActiveRecord 
+abstract class ActiveRecordModel extends ActiveRecord
 {
 }
 
