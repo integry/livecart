dd2405e1ddc6815c6ad072c5d6819c44e03e9153
rinalds <devnull@localhost>
2007-01-22 16:40:28 +0200

    [svn]

--- test/unit/model/TestProduct.php
+++ test/unit/model/TestProduct.php
@@ -6,24 +6,43 @@ require_once("init.php");
 ClassLoader::import("application.model.category.*");
 ClassLoader::import("application.model.product.*");
 
+// create a new category
 $productCategory = Category::getNewInstance(Category::getRootNode());
 $productCategory->setValueByLang("name", "en", "Demo category branch");
 $productCategory->save();
 
+// create a product without attributes
 $product = Product::getNewInstance($productCategory);
 $product->setValueByLang("name", "en", "Test product...");
 $product->setValueByLang("name", "lt", "Bandomasis produktas");
 $product->setFieldValue("isEnabled", true);
-
 $product->save();
 
+// create some attributes
+$numField = SpecField::getNewInstance($productCategory, SpecField::DATATYPE_NUMBERS, SpecField::TYPE_NUMBERS_SIMPLE);
+$numField->handle->set('numeric.field');
+$numField->setValueByLang('name', 'en', 'This would be a numeric field');
+$numField->setValueByLang('name', 'lt', 'Cia galima rasyt tik skaicius');
+$numField->save();
 
+$textField = SpecField::getNewInstance($productCategory, SpecField::DATATYPE_TEXT, SpecField::TYPE_TEXT_SIMPLE);
+$textField->handle->set('text.field');
+$textField->setValueByLang('name', 'en', 'Here goes some free text');
+$textField->setValueByLang('name', 'lt', 'Cia bet ka galima irasyt');
+$textField->save();
+
+// assign attribute values for product
+$product->setSpecFieldValue($numField->getID(), 666);
+$product->save();
+
+/*
 $productSet = ActiveRecord::getRecordSet("Product", new ARSelectFilter());
 print_r($productSet->toArray());
 
 $filterSet = Filter::getRecordSetArray(new ARSelectFilter());
 
 print_r($filterSet);
+*/
 
 echo "OK\n<br/>";
 echo "</pre>";
