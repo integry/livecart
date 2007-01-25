129e4a1c07a59448e39ee127a47477cdc27e8a43
rinalds <devnull@localhost>
2007-01-25 19:05:07 +0200

    [svn]

--- public/javascript/backend/Backend.js
+++ public/javascript/backend/Backend.js
@@ -5,6 +5,39 @@ function showHelp(url)
 
 var Backend = {};
 
+// set default locale
+Backend.locale = 'en';
+
+/*************************************************
+	onLoad handler
+**************************************************/
+Backend.onLoad = function()
+{
+	// initialize fckEditor (WYSIWYG textareas)
+	var sBasePath = 'javascript/library/fckeditor/';
+	editors = document.getElementsByTagName('textarea');
+	for (k = 0; k < editors.length; k++)
+	{
+		if (Element.hasClassName(editors[k], 'fckEditor'))
+		{
+			var oFCKeditor = new FCKeditor( editors[k].name ) ;
+
+			oFCKeditor.Config["AutoDetectLanguage"]	= false;
+			oFCKeditor.Config["DefaultLanguage"] = Backend.locale;	
+
+			oFCKeditor.BasePath	= sBasePath ;
+			oFCKeditor.ToolbarSet = 'Default';
+			oFCKeditor.Width = '600';
+			oFCKeditor.ReplaceTextarea();	  			  
+		}
+	}	
+
+	// AJAX navigation
+	dhtmlHistory.initialize();
+	dhtmlHistory.addListener(Backend.ajaxNav.handle);
+	dhtmlHistory.handleBookmark();
+}	
+
 /*************************************************
 	AJAX back/forward navigation
 **************************************************/
