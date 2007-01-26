c10d4a0e21847daa5569f7e65a35297c98ff67ad
rinalds <devnull@localhost>
2007-01-26 15:36:17 +0200

    [svn] testing issue #32 ssssss sss

--- public/javascript/backend/Language.js
+++ public/javascript/backend/Language.js
@@ -315,7 +315,7 @@ Backend.LanguageEdit.prototype =
 				{
 					langEdit.langToggleVisibility(this.parentNode);	
 				}
-				// full expand ... another test .. still testing ... should work..really..now...hmm
+				// full expand ... another test .. still testing ... should work..really..now...hmm.uhh
 				else
 				{
 					langEdit.langExpandAll(this.parentNode.parentNode.id, 1 - langEdit.isContainerVisible(this.parentNode.	parentNode.parentNode));				  
