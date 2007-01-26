7dd6c27d57a62c6e1a76f74bf06e037346a384f5
rinalds <devnull@localhost>
2007-01-26 15:25:49 +0200

    [svn] fixed #32 - testing integration...

--- public/javascript/backend/Language.js
+++ public/javascript/backend/Language.js
@@ -315,7 +315,7 @@ Backend.LanguageEdit.prototype =
 				{
 					langEdit.langToggleVisibility(this.parentNode);	
 				}
-				// full expand ... another test .. still testing
+				// full expand ... another test .. still testing ... should work
 				else
 				{
 					langEdit.langExpandAll(this.parentNode.parentNode.id, 1 - langEdit.isContainerVisible(this.parentNode.	parentNode.parentNode));				  
