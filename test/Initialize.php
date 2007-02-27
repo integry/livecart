bf95665bdf41244838862a2b3131d672b60a2ac7
rinalds <devnull@localhost>
2007-02-27 13:32:47 +0200

    [svn]

--- test/Initialize.php
+++ test/Initialize.php
@@ -1,18 +1,48 @@
 <?php
 
-// setup test database
-require_once('init/SetupTestDb.php');
-SetupTestDb('server', 'root', '', 'K-shop', 'livecart_test');
+error_reporting(E_ALL);
+	
+if (!defined('TEST_INITIALIZED'))
+{
+	// load classes and mount paths
+	$cd = getcwd();
+	
+	chdir(dirname(__FILE__));
+	chdir('..');
+	require_once('framework/ClassLoader.php');
 
-// load/import classes
-require_once('init/LoadClasses.php');
-
-abstract class BSModel extends ActiveRecordModel {}
-
-ActiveRecord::setDSN("mysql://root@192.168.1.6/livecart_test");
-
-// start application
-//require_once('../helper/StartApplication.php');
+	// set unittest and simpletest library root directory
+	$libDir = dirname(dirname(__FILE__)) . '/library/';
+	echo $libDir;
+	ClassLoader::mountPath('simpletest', realpath($libDir . 'simpletest/'));
+	ClassLoader::mountPath('unittest', realpath($libDir . 'unittest') . '/');
+	ClassLoader::mountPath('testdir', dirname(__FILE__).'/');
+	
+	ClassLoader::mountPath('framework', dirname(dirname(dirname(__file__))).'/framework/');
+	ClassLoader::mountPath('application', dirname(dirname(dirname(__file__))).'/application/');
+	ClassLoader::mountPath('library', dirname(dirname(dirname(__file__))).'/library/');
+	ClassLoader::import("framework.*");
+	ClassLoader::import("framework.request.Request");
+	ClassLoader::import("framework.request.Router");
+	ClassLoader::import("framework.renderer.TemplateRenderer");
+	ClassLoader::import("framework.controller.*");
+	ClassLoader::import("framework.response.*");
+	ClassLoader::import("application.controller.*");
+	ClassLoader::import("application.model.*");
+	ClassLoader::import("simpletest.*");
+	ClassLoader::import("unittest.*");
+	ClassLoader::import("testdir.*");
+	
+	ClassLoader::load('unit_tester');
+	ClassLoader::load('mock_objects');
+	ClassLoader::load('reporter');
+	
+	ClassLoader::import('unittest.UnitTest');
+	
+	chdir($cd);
+	
+	define('TEST_INITIALIZED', true);
+}
 
 require_once('UTStandalone.php');
 
---
+++ test/model/system/MultiLingualObject.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once('../../../Initialize.php');
+
+ClassLoader::import('application.model.locale.Language');
+
+/**
+ * Language model test
+ * @author Integry Systems
+ * @package application.test.model.system
+ */
+class TestMultiLingualObject extends UnitTest
+{	  
+
+
+}
+
+?>
