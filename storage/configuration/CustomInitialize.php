0f1940bd3f7ac411192bae83fc0e86a49ac798a2
rinalds <devnull@localhost>
2007-05-21 21:19:39 +0300

    [svn]

--- storage/configuration/CustomInitialize.php
+++ storage/configuration/CustomInitialize.php
@@ -1,5 +1,7 @@
 <?php
 
+// ClassLoader::mountPath('application.view.layout.frontend', )
+
 ///ClassLoader::mountPath('application.controller.IndexController*php', dirname(__file__));
 
 ///ClassLoader::overrideFilePath('application.controller', 'IndexController')
