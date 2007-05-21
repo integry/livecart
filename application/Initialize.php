81549f7fff89a7e11bc45fa036d0ae891fc01422
rinalds <devnull@localhost>
2007-05-21 16:14:27 +0300

    [svn]

--- application/Initialize.php
+++ application/Initialize.php
@@ -4,24 +4,31 @@
  *  Initialize framework, load main classes
  */ 
 
-require_once(dirname(dirname(__file__)) . DIRECTORY_SEPARATOR . "framework" . DIRECTORY_SEPARATOR . "ClassLoader.php");
+require_once(dirname(dirname(__file__)) . DIRECTORY_SEPARATOR . 'framework' . DIRECTORY_SEPARATOR . 'ClassLoader.php');
 
-ClassLoader::mountPath(".", dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR);
+ClassLoader::mountPath('.', dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR);
 
-ClassLoader::import("library.stat.Stat");
+ClassLoader::import('library.stat.Stat');
 $stat = new Stat(true);
 
-ClassLoader::import("framework.request.Request");
-ClassLoader::import("framework.request.Router");
-ClassLoader::import("framework.renderer.TemplateRenderer");
-ClassLoader::import("framework.controller.*");
-ClassLoader::import("framework.response.*");
-ClassLoader::import("application.controller.*");
-ClassLoader::import("application.model.*");
-ClassLoader::import("application.model.system.*");
+ClassLoader::import('framework.request.Request');
+ClassLoader::import('framework.request.Router');
+ClassLoader::import('framework.renderer.TemplateRenderer');
+ClassLoader::import('framework.controller.*');
+ClassLoader::import('framework.response.*');
+ClassLoader::import('application.controller.*');
+ClassLoader::import('application.model.*');
+ClassLoader::import('application.model.system.*');
 
 // LiveCart request routing rules
-ClassLoader::import("application.configuration.route.backend");
-TemplateRenderer::setCompileDir(ClassLoader::getRealPath("cache.templates_c"));
+ClassLoader::import('application.configuration.route.backend');
+TemplateRenderer::setCompileDir(ClassLoader::getRealPath('cache.templates_c'));
+
+// Custom initialization tasks
+$custom = ClassLoader::getRealPath('storage.configuration.CustomInitialize') . '.php';
+if (file_exists($custom))
+{
+    ClassLoader::import('storage.configuration.CustomInitialize');    
+}
 
 ?>
---
+++ storage/configuration/CustomInitialize.php
@@ -0,0 +1,7 @@
+<?php
+
+///ClassLoader::mountPath('application.controller.IndexController*php', dirname(__file__));
+
+///ClassLoader::overrideFilePath('application.controller', 'IndexController')
+
+?>
