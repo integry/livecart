e610474751d59a11dde7de5c0bed44b0dcfd68fd
Rinalds Uzkalns <devnull@localhost>
2007-09-13 11:09:05 +0300

    Forgotten files

---
+++ public/appdir.php
@@ -0,0 +1,9 @@
+<?php
+
+	// application directories are in the same directory as public
+	return dirname(dirname(__file__));
+
+	// comment out the above line if the application directory is different
+	return '/home/www';
+
+?>
