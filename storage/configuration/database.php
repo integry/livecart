64a41ca3b892ae840ddb15a64834b071b34e68c6
Rinalds Uzkalns <devnull@localhost>
2007-09-13 03:11:35 +0300

    Restored database configuration

--- storage/configuration/database.php
+++ storage/configuration/database.php
@@ -1,5 +1,5 @@
 <?php return array (
   'production' => 'mysql://root@localhost/livecart_prex',
-  'development' => 'mysql://root@localhost/livecart_prex',
-  'test' => 'mysql://root@localhost/livecart_prex',
+  'development' => 'mysql://root@localhost/livecart_development',
+  'test' => 'mysql://root@localhost/livecart_test',
 ); ?>
