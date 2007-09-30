381373c7b51cb5f2fa8e0aa0203acfdaaa8615f0
root@host.turn-k.net <root@host.turn-k.net>
2007-09-30 17:23:03 -0400

    Package build script

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
-#!/build/sh
-$MAIN=/var/db/repo/livecart
-$BUILD=/var/db/repo/build
-$TMP=/tmp/build
+#!/bin/bash
+MAIN=/home/mercurial/repo/livecart
+BUILD=/home/mercurial/repo/build
+TMP=/tmp/build
+PACKAGE=/var/db/livecart
 
 # get last log message
 cd $MAIN
@@ -14,14 +15,39 @@ find $TMP -name '.hg' | xargs rm -rf
 
 # copy all files to build repo
 mv $BUILD/.hg $TMP/.hg
+mv $BUILD/.hgtags $TMP/.hgtags
 rm -rf $BUILD
 mv $TMP $BUILD
 cd $BUILD
 
 # get version
-$VERSION=`head .version`
+VERSION=`head .version`
 
 # commit changes
 hg addremove
 hg commit -m "$VERSION:"$'\n'"$LOG"
 hg tag $VERSION
+
+# prepare build
+rm -rf $TMP
+cp -rf $BUILD $TMP
+cd $TMP
+
+rm -rf .hg*
+rm -rf .snap
+rm -rf build.sh
+rm -rf doc
+
+mkdir cache
+mkdir storage
+mkdir public/cache
+
+# create package files
+TAR=$PACKAGE/$VERSION.tar
+rm -rf $TAR.gz
+tar cf $TAR *
+gzip -9 $TAR
+
+ZIP=$PACKAGE/$VERSION.zip
+rm -rf $ZIP
+zip -rq $ZIP *
