e4d4530726b47f171e5c211a6b4108df661c6e6e
Rinalds Uzkalns <devnull@localhost>
2007-10-06 22:56:45 +0300

    Fixed interpreter paths for build shell scripts

--- build/copyrightJs.sh
+++ build/copyrightJs.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 cat ./build/copyright.js $1 > $1.copy
 rm $1
 mv $1.copy $1
--- build/copyrightPhp.sh
+++ build/copyrightPhp.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 cat ./build/copyright.php $1 > $1.copy
 rm $1
 mv $1.copy $1
