6c7410b425eeebeb9b4908bb71b273349f1f710f
Rinalds Uzkalns <devnull@localhost>
2007-10-09 16:16:20 +0300

    Page URLs with comma in filter names didn't work

--- application/helper/CreateHandleString.php
+++ application/helper/CreateHandleString.php
@@ -3,7 +3,7 @@
 /**
  * Creates a handle string that is usually used as part of URL to uniquely
  * identify some record.
- * 
+ *
  * Basically it simply removes reserved URL characters and does some basic formatting
  *
  * @param string $str
@@ -18,16 +18,16 @@ function createHandleString($str)
 	{
         $str = array_shift($str);
     }
-    
-    $str = str_replace(array('$', '&', '+', '/', '\\', ':', ';', '=', '?', '@', '.', ' ', '"', "'", '#', '*', '>', '<'), '-', $str);
+
+    $str = str_replace(array('$', '&', '+', '/', '\\', ':', ';', '=', '?', '@', '.', ' ', '"', "'", '#', '*', '>', '<', ','), '-', $str);
 
 	$str = preg_replace('/-{2,}/', '-', $str);
 	$str = preg_replace('/^-/', '', $str);
 	$str = preg_replace('/-$/', '', $str);
-	
+
 	$str = urlencode($str);
-	
+
 	return $str;
 }
-	
+
 ?>
