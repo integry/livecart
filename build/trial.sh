c4222ea3bc6b1e160de2ec0a7083246250a0b54c
root@host.turn-k.net <root@host.turn-k.net>
2007-10-24 15:08:17 -0400

    Trial package build script completed

--- build/trial.sh
+++ build/trial.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 # get the LiveCart version number
 VERSION=`head /home/livecart/public_html/update/.version`
 
@@ -11,6 +9,7 @@ cp /var/db/livecart/livecart-$VERSION.tar.gz /tmp/build.tar.gz
 
 # unpack
 rm -rf /tmp/build /tmp/encoded
+mkdir /tmp/build
 cd /tmp/build
 tar -xzf /tmp/build.tar.gz
 
@@ -18,10 +17,12 @@ tar -xzf /tmp/build.tar.gz
 cp -r /home/livecart/ioncube /tmp/build
 
 # copy readme file
-cp -rf /home/mercurial/repo/livecart/trialReadme.txt /tmp/build/readme.txt
+cp -rf /home/mercurial/repo/livecart/build/trialReadme.txt /tmp/build/readme.txt
 
 # encode
 /home/turnk/public_html/ion/ioncube_encoder5 --replace-target --expire-in 15d --add-comment "LiveCart Trial Version" --action-if-no-loader="echo(\'No ionCube loader is installed. The loader is necessary to run the encoded trial version files. <a href=ioncube/ioncube-loader-helper.php>See further instructions for installing the loader</a>.<br/><br/>The loader will not be necessary for the paid version as it is shipped with full source code.\');exit (199);" /tmp/build -o /tmp/encoded
+
+set -e
 
 # create archive packages
 cd /tmp/encoded
@@ -41,3 +42,5 @@ cp -rf /tmp/trial.zip /home/livecart/public_html/trial/livecart-trial.zip
 
 # remove all files
 rm -rf /tmp/build /tmp/encoded /tmp/trial*
+
+echo 'Trial packages built successfuly'
