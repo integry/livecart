b64734a1590ec72feb00d191fb43c693c45873b7
Rinalds <rinalds@rinalds-laptop.integry>
2008-08-28 21:33:04 +0300

    ActiveGrid date filtering (custom date ranges not implemented yet)

--- application/helper/smarty/function.activeGrid.php
+++ application/helper/smarty/function.activeGrid.php
@@ -6,29 +6,32 @@
  * @param array $params
  * @param Smarty $smarty
  * @return string
- * 
+ *
  * @package application.helper.smarty
  * @author Integry Systems
  */
-function smarty_function_activeGrid($params, LiveCartSmarty $smarty) 
+function smarty_function_activeGrid($params, LiveCartSmarty $smarty)
 {
 	if (!isset($params['rowCount']) || !$params['rowCount'])
 	{
-		$params['rowCount'] = 15;	
+		$params['rowCount'] = 15;
 	}
-	
+
 	foreach ($params as $key => $value)
 	{
 		$smarty->assign($key, $value);
 	}
-	
+
 	if (isset($params['filters']) && is_array($params['filters']))
 	{
 		$smarty->assign('filters', $params['filters']);
 	}
-	
+
 	$smarty->assign('url', $smarty->getApplication()->getRouter()->createUrl(array('controller' => $params['controller'], 'action' => $params['action']), true));
-	
+
+	$smarty->assign('thisMonth', date('m'));
+	$smarty->assign('lastMonth', date('Y-m', strtotime(date('m') . '/15 -1 month')));
+
 	return $smarty->display('block/activeGrid/gridTable.tpl');
 }
 
