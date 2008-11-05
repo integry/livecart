305883a05ac175abe1328dc3f17fe0ba1ff1aa5a
Rinalds Uzkalns <rinalds@integry.net>
2008-11-05 18:12:09 +0200

    Fixed: filter translations were not saved

--- application/controller/backend/FilterGroupController.php
+++ application/controller/backend/FilterGroupController.php
@@ -92,7 +92,6 @@ class FilterGroupController extends StoreManagementController
 		{
 			$filters = $this->request->get('filters', false);
 
-			//$filterGroup->setLanguageField('name',  $name, $this->filtersConfig['languageCodes']);
 			$filterGroup->loadRequestData($this->request);
 			$filterGroup->specField->set(SpecField::getInstanceByID((int)$this->request->get('specFieldID')));
 			$filterGroup->save();
