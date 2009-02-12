47e23e5d720b13d2f2b0b642178ec438206394c4
Rinalds Uzkalns <rinalds@integry.net>
2009-02-13 00:09:56 +0200

    Fixed PHP error messages in the Order History tab

--- application/controller/backend/OrderLogController.php
+++ application/controller/backend/OrderLogController.php
@@ -14,24 +14,28 @@ class OrderLogController extends StoreManagementController
 {
 	public function index()
 	{
-		$response = new ActionResponse();		
+		$response = new ActionResponse();
 		$customerOrder = CustomerOrder::getInstanceById($this->request->get('id'), true, array('User', 'Currency'));
 
 		$logs = array();
 		foreach(OrderLog::getRecordSetByOrder($customerOrder, null, array('User'))->toArray() as $entry)
 		{
+			if (!$entry['oldValue'])
+			{
+				$entry['oldValue'] = array();
+			}
+
 			if($entry['action'] != OrderLog::ACTION_REMOVED_WITH_SHIPMENT)
 			{
 				$logs[] = $entry;
 				$logs[count($logs) - 1]['items'] = array();
-			} 
+			}
 			else
 			{
 				$logs[count($logs) - 1]['items'][] = $entry;
 			}
 		}
-		
-		
+
 		$response->set('defaultCurrencyCode', $this->application->getDefaultCurrencyCode());
 		$response->set('logs', $logs);
 		return $response;
