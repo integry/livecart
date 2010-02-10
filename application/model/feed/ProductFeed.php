0c11f24411064db5ffb97398d0a8e28c791cac9b
Rinalds Uzkalns <rinalds@integry.net>
2010-02-11 00:17:35 +0200

    Product XML export allows to include full category paths

--- application/model/feed/ProductFeed.php
+++ application/model/feed/ProductFeed.php
@@ -24,6 +24,7 @@ class ProductFeed extends ARFeed
 	{
 		ProductPrice::loadPricesForRecordSetArray($this->data);
 		ProductSpecification::loadSpecificationForRecordSetArray($this->data, true);
+		Product::loadCategoryPathsForArray($this->data);
 	}
 }
 
