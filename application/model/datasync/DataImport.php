18f660921b86301f78edb4b483d2f28a6993bca9
Rinalds Uzkalns <rinalds@integry.net>
2010-05-28 14:36:49 +0300

    Added ability to import orders via CSV (including ordered products, customer info and addresses)

--- application/model/datasync/DataImport.php
+++ application/model/datasync/DataImport.php
@@ -67,12 +67,12 @@ abstract class DataImport
 			if (method_exists($this, 'set_' . $className . '_' . $field))
 			{
 				$method = 'set_' . $className . '_' . $field;
-				$this->$method($instance, $value);
+				$this->$method($instance, $value, $record, $profile);
 			}
 			else if (method_exists($this, 'set_' . $field))
 			{
 				$method = 'set_' . $field;
-				$this->$method($instance, $value);
+				$this->$method($instance, $value, $record, $profile);
 			}
 			else if (isset($instance->$field) && ($instance->$field instanceof ARValueMapper) && ($className == $this->getClassName()))
 			{
@@ -87,6 +87,7 @@ abstract class DataImport
 		foreach ($this->getReferencedData() as $section)
 		{
 			$method = 'import_' . $section;
+
 			if (method_exists($this, $method))
 			{
 				$subProfile = $profile->extractSection($section);
@@ -114,12 +115,16 @@ abstract class DataImport
 		return $id;
 	}
 
-	public function importRelatedRecord($type, ActiveRecordModel $instance, $record, CsvImportProfile $profile)
+	protected function getImporterInstance($type)
 	{
 		$class = $type . 'Import';
-		$this->application->loadPluginClass('application.model.datasync', $class);
-		$import = new $class($this->application);
-		return $import->importInstance($record, $profile, $instance);
+		$this->application->loadPluginClass('application.model.datasync.import', $class);
+		return new $class($this->application);		
+	}
+	
+	public function importRelatedRecord($type, ActiveRecordModel $instance, $record, CsvImportProfile $profile)
+	{
+		return $this->getImporterInstance($type)->importInstance($record, $profile, $instance);
 	}
 
 	public function disableRecords(ARSelectFilter $filter)
@@ -418,6 +423,14 @@ abstract class DataImport
 	{
 		preg_match('/(.*)Import/', get_class($this), $match);
 		return array_pop($match);
+	}
+	
+	public function getColumnValue($record, CsvImportProfile $profile, $fieldName)
+	{
+		if ($profile->isColumnSet($fieldName))
+		{
+			return $record[$profile->getColumnIndex($fieldName)];
+		}
 	}
 
 	public function setFlushMessage($msg)
