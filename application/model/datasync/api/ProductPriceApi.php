68f1cb0356197a03d16c441dc7f21aa04e61bb03
Nauris Vilcmeirs <nauris.vilcmeirs@gmail.com>
2010-05-31 12:28:00 +0300

    ProducPriceApi, get action.

--- application/model/datasync/api/ProductPriceApi.php
+++ application/model/datasync/api/ProductPriceApi.php
@@ -1,7 +1,10 @@
 <?php
 
 ClassLoader::import('application.model.datasync.ModelApi');
+
+ClassLoader::import('application.model.product.Product');
 ClassLoader::import('application.model.product.ProductPrice');
+
 ClassLoader::import('application.model.datasync.api.reader.XmlPriceApiReader');
 ClassLoader::import('application.helper.LiveCartSimpleXMLElement');
 	
@@ -42,7 +45,79 @@ class ProductPriceApi extends ModelApi
 	public function get()
 	{
 		$request = $this->getApplication()->getRequest();
-		echo $request->get('SKU');
+
+		$product = Product::getInstanceBySku($request->get('SKU'));
+		if($product == null)
+		{
+			throw new Exception('Product not found');
+		}
+		
+		$products = array($product->toArray());
+		ProductPrice::loadPricesForRecordSetArray($products);
+		
+		
+		
+		$response = new LiveCartSimpleXMLElement('<response datetime="'.date('c').'"></response>');
+		foreach($products as $product)
+		{
+			$this->fillSimpleXmlResponseItem($response, $product);
+		}
+		
+		
+		return new SimpleXMLResponse($response);
+		
+		
+	}
+	
+	public function fillSimpleXmlResponseItem($xml, $product)
+	{
+		// product info
+		$fieldNames = array('sku');
+		foreach($fieldNames as $fieldName)
+		{
+			$xml->addChild($fieldName, $product[$fieldName]);
+		}
+		
+		// pricing info
+		
+		
+		foreach(array('definedPrices', 'definedListPrices') as $key)
+		{
+			if(array_key_exists($key, $product))
+			{
+				$xmlGrop = $xml->AddChild($key);
+				foreach($product[$key] as $currency => $value)
+				{
+					$xmlGrop->addChild($currency, $value);
+				}
+			}
+		}
+
+		$xmlGrop = $xml->addChild('quantityPrices');
+		
+		if(array_key_exists('prices', $product) && is_array($product['prices']))
+		{
+			foreach($product['prices'] as $currency => $pricingDetails)
+			{
+				if(array_key_exists('quantityPrices', $pricingDetails))
+				{
+					foreach($pricingDetails['quantityPrices'] as $quantityPrice)
+					{
+						foreach($quantityPrice as $quantity=>$item)
+						{
+							$xmlQp = $xmlGrop->addChild('quantityPrice');
+							$xmlQp->addChild('currency', $currency);
+							foreach(array(/*'originalPrice',*/'price','from','to') as $itemFieldName)
+							{
+								$xmlQp->addChild($itemFieldName, $item[$itemFieldName]);
+							}
+						}
+					}
+				}
+			}
+		}
+
+		//pp($product);
 	}
 }
 
