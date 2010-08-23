2f3f556ba1ac5d25a9cc2f304c67d9e22447b368
Nauris Vilcmeirs <nauris.vilcmeirs@gmail.com>
2010-08-23 18:15:00 +0300

    Ability to edit tax rates from taxes form.

--- application/controller/backend/TaxController.php
+++ application/controller/backend/TaxController.php
@@ -35,22 +35,18 @@ class TaxController extends StoreManagementController
 		$response->set("newTaxForm", $this->createTaxForm($newTax));
 		$response->set("newTax", $newTax->toArray());
 
-		return $response;
+		return $this->appendTaxRates($response);
 	}
 
 	public function edit()
 	{
 		$tax = Tax::getInstanceByID((int)$this->request->get('id'), true);
-
 		$form = $this->createTaxForm($tax);
 		$form->setData($tax->toArray());
-
-
 		$response = new ActionResponse();
 		$response->set('tax', $tax->toArray());
 		$response->set('taxForm', $form);
-
-		return $response;
+		return $this->appendTaxRates($response, $tax->getID());
 	}
 
 	/**
@@ -69,8 +65,8 @@ class TaxController extends StoreManagementController
 	 */
 	public function update()
 	{
-		$tax = Tax::getInstanceByID((int)$this->request->get('id'));
-
+		$request = $this->getRequest();
+		$tax = Tax::getInstanceByID((int)$request->get('id'));
 		return $this->saveTax($tax);
 	}
 
@@ -88,22 +84,20 @@ class TaxController extends StoreManagementController
 	private function saveTax(Tax $tax)
 	{
 		$validator = $this->createTaxFormValidator($tax);
-
 		if($validator->isValid())
 		{
 			$tax->setValueArrayByLang(array('name'), $this->application->getDefaultLanguageCode(), $this->application->getLanguageArray(true, false), $this->request);
-
 			$tax->save();
-
+			$this->saveTaxRates($tax);
 			return new JSONResponse(array('tax' => $tax->toArray()), 'success');
 		}
 		else
 		{
-
 			return new JSONResponse(array('errors' => $validator->getErrorList()), 'failure', $this->translate('_could_not_save_tax_entry'));
 		}
 	}
-
+	
+	
 	/**
 	 * @return Form
 	 */
@@ -124,6 +118,18 @@ class TaxController extends StoreManagementController
 		$validator = $this->getValidator("taxForm_" . $tax->isExistingRecord() ? $tax->getID() : '', $this->request);
 		$validator->addCheck("name", new IsNotEmptyCheck($this->translate("_error_the_name_should_not_be_empty")));
 
+		$zones = DeliveryZone::getAll();
+		$zones->add(DeliveryZone::getDefaultZoneInstance());
+		$classes = TaxClass::getAllClasses();
+		$this->loadLanguageFile('backend/TaxRate'); // tax rate error messages
+		foreach($zones as $zone)
+		{
+			$this->appendTaxRateFieldValidator($validator, $zone);
+			foreach($classes as $class)
+			{
+				$this->appendTaxRateFieldValidator($validator, $zone, $class);
+			}
+		}
 		return $validator;
 	}
 
@@ -141,6 +147,118 @@ class TaxController extends StoreManagementController
 
 		return new JSONResponse(false, 'success');
 	}
+
+	private function appendTaxRates($response, $taxID = null)
+	{
+		$this->loadLanguageFile('backend/DeliveryZone'); // 'default zone'
+
+		$zones = array();
+		//default
+		$taxRates = TaxRate::getRecordSetByDeliveryZone(null);
+		$zones[] = array(
+			'ID' => -1,
+			'name' => $this->translate('_default_zone'),
+			'taxRates' => $taxRates ? $taxRates->toArray() : null
+		);
+
+		// custom
+		foreach(DeliveryZone::getAll() as $deliveryZone)
+		{
+			$zone = $deliveryZone->toArray();
+			$taxRates = $deliveryZone->getTaxRates();
+			$zones[] = array(
+				'ID' => $zone['ID'],
+				'name' => $zone['name'],
+				'taxRates' => $taxRates ? $taxRates->toArray() : null
+			);
+		}
+
+		// reorder tax rates in structure $zone['taxRates'][<delivery zone id>][<tax class id>] = ..
+		//                                (for default delivery zone and tax class id use -1)
+		foreach($zones as &$zone)
+		{
+			$filtered = array();
+			if(is_array($zone['taxRates']))
+			{
+				foreach($zone['taxRates'] as $taxRate)
+				{
+					if($taxRate['Tax']['ID'] == $taxID)
+					{
+						$filtered[$zone['ID']][array_key_exists('taxClassID',$taxRate) ? $taxRate['taxClassID'] : -1] = $taxRate;
+					}
+				}
+			}
+			$zone['taxRates'] = $filtered;
+			//pp($zone['taxRates']);
+		}
+		$classes = TaxClass::getAllClasses()->toArray();
+
+		$response->set('zones', $zones);
+		$response->set('classes', $classes);
+		return $response;
+	}
+
+	private function saveTaxRates(Tax $tax)
+	{
+		$zones = DeliveryZone::getAll();
+		$zones->add(DeliveryZone::getDefaultZoneInstance());
+		$classes = TaxClass::getAllClasses();
+		ActiveRecord::beginTransaction();
+		foreach($zones as $zone)
+		{
+			// delete all zone tax rates
+			$taxRates = $zone->getTaxRates();
+			foreach ($taxRates as $rate)
+			{
+				if($rate->taxID->get()->getID() == $tax->getID())
+				{
+					$rate->delete();
+				}
+			}
+			$this->saveRate($zone, $tax);
+			foreach($classes as $class)
+			{
+				$this->saveRate($zone, $tax, $class);
+			}
+		}
+		ActiveRecord::commit();
+	}
+
+	private function saveRate(DeliveryZone $zone, Tax $tax, TaxClass $class = null)
+	{
+		$value = $this->request->get($this->getFieldName($zone, $class));
+		if (!is_null($value) && ($value !== ''))
+		{
+			$taxRate = TaxRate::getNewInstance($zone, $tax, $value);
+			$taxRate->taxClass->set($class);
+			$taxRate->save();
+			return $taxRate;
+		}
+	}
+
+	private function appendTaxRateFieldValidator(LiveCartValidator $validator, DeliveryZone $zone, TaxClass $class = null)
+	{
+		$field = $this->getFieldName($zone, $class);
+		$validator->addCheck($field, new IsNumericCheck($this->translate("_error_rate_should_be_numeric_value")));
+		$validator->addCheck($field, new MinValueCheck($this->translate("_error_rate_should_be_greater_than_zero"), 0));
+		$validator->addFilter($field, new NumericFilter());
+	}
+
+	private function getFieldName(DeliveryZone $zone = null, TaxClass $class = null)
+	{
+		$classID = $class ? $class->getID() : '-1';
+		$zoneID = $zone ? $zone->getID() : '-1';
+		if($classID == 0)
+		{
+			$classID = -1;
+		}
+		if($zoneID == 0)
+		{
+			$zoneID = -1;
+		}
+		return 'taxRate_' . $zoneID . '_' . $classID;
+	}
+
 }
 
 ?>
--- application/view/backend/tax/tax.tpl
+++ application/view/backend/tax/tax.tpl
@@ -5,10 +5,7 @@
 {/if}
 
 {form handle=$taxForm action=$action id="taxForm_`$tax.ID`" method="post" onsubmit="Backend.Tax.prototype.getInstance(this).save(); return false;" role="taxes.update(edit),taxes.create(index)"}
-
 	{hidden name="ID"}
-
-
 	<p>
 		<label>{t _name}</label>
 		<fieldset class="error">
@@ -16,7 +13,6 @@
 			<div class="errorText" style="display: none"></div>
 		</fieldset>
 	</p>
-
 	{language}
 		<label>{t _name}</label>
 		<fieldset class="error">
@@ -25,6 +21,48 @@
 		</fieldset>
 	{/language}
 
+	<fieldset class="taxZonesContainer">
+		{*
+			for some reason smarty does not understand $foo[1][-1], but $foo[1][$bar] where $bar value -1 will work.
+			Define variable containing -1
+		*}
+		{assign var="Default" value="-1"}
+		<table class="taxZones">
+			<tbody>
+				{foreach $zones as $zone}
+					<tr>
+						<td class="zoneName">
+							{if $zone.ID < 1}
+								<h3>{t _base_tax_rate}</h3>
+								<p>{t _base_tax_rates_description}</p>
+							{else}
+								<h3>{$zone.name}</h3>
+							{/if}
+						</td>
+						<td>
+							{textfield class="number" value=$zone.taxRates[$zone.ID][$Default].rate|default:0 name="taxRate_`$zone.ID`_`$Default`"} %
+							<div class="errorText" style="display: none"></div>
+							{if count($classes)}
+								<table class="taxClass">
+									<tbody>
+										{foreach $classes as $class}
+											<tr>
+												<td>{$class.name_lang}</td>
+												<td>
+													{textfield class="number" value=$zone.taxRates[$zone.ID][$class.ID].rate|default:0 name="taxRate_`$zone.ID`_`$class.ID`"} %
+													<div class="errorText" style="display: none"></div>
+												</td>
+											</tr>
+										{/foreach}
+									</tbody>
+								</table>
+							{/if}
+						</td>
+					</tr>
+				{/foreach}
+			</tbody>
+		</table>
+	</fieldset>
 	<fieldset class="tax_controls controls">
 		<span class="progressIndicator" style="display: none;"></span>
 		<input type="submit" class="
@@ -32,5 +70,6 @@
 		{t _or}
 		<a href="#cancel" class="tax_cancel cancel">{t _cancel}</a>
 	</fieldset>
+
 
 {/form}
