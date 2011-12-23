445fe1ff48b59a45343214952d2eacac07dbbd78
Rinalds Uzkalns <rinalds@integry.net>
2011-12-23 19:31:20 +0200

    Minor frontend admin functions Javascript fix
    Conflicts:
    
    	public/javascript/frontend/Admin.js

---
+++ public/javascript/frontend/Admin.js
@@ -0,0 +1,44 @@
+;(function()
+{
+	var $ = jQuery;
+
+	var getEditLink = function(h1)
+	{
+		$(h1).addClass('edit');
+
+		var span = document.createElement('span');
+		span.innerHTML = h1.innerHTML;
+		$(h1).empty();
+		h1.appendChild(span);
+
+		var edit = document.createElement('a');
+		edit.className = 'edit';
+		edit.target = '_blank';
+		h1.appendChild(edit);
+
+		return edit;
+	}
+
+	var getRecordId = function(container, prefix)
+	{
+		return (new RegExp(prefix + '([0-9]+)')).exec(container.attr('class')).pop();
+	}
+
+	$(window).load(function(e)
+	{
+		$('.product-index h1').each(function(index, h1)
+		{
+			getEditLink(h1).href = Router.createUrl('backend.category', 'index') + '#product_' + getRecordId($('.productIndex'), 'product_');
+		});
+
+		$('.category-index h1').each(function(index, h1)
+		{
+			getEditLink(h1).href = Router.createUrl('backend.category', 'index') + '#cat_' + getRecordId($('.categoryIndex'), 'category_') + '#tabMainDetails__';
+		});
+
+		$('.staticPage-view h1').each(function(index, h1)
+		{
+			getEditLink(h1).href = Router.createUrl('backend.staticPage', 'index') + '#page_' + getRecordId($('.staticPageView'), 'staticPage_');
+		});
+	});
+})();
