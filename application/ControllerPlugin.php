27c0f17ff2d55bb5dd8b34d08d5ef259fed4eae4
Rinalds Uzkalns <rinalds@integry.net>
2013-10-17 01:30:54 +0300

    ControllerPlugin class adjustments

--- application/ControllerPlugin.php
+++ application/ControllerPlugin.php
@@ -6,41 +6,18 @@
  *  @package application
  *  @author Integry Systems
  */
-abstract class ControllerPlugin
+abstract class ControllerPlugin extends \Phalcon\DI\Injectable
 {
 	private $mustStop;
 
-	protected $response;
-
 	protected $controller;
-
-	protected $request;
-
-	protected $controllerName;
-
-	protected $actionName;
 
 	public abstract function process();
 
-	public function __construct(Response $response, Controller $controller)
+	public function __construct(ControllerBase $controller, \Phalcon\DI\FactoryDefault $di)
 	{
-		$this->response = $response;
+		$this->setDI($di);
 		$this->controller = $controller;
-		$this->request = $controller->getRequest();
-		$this->application = $controller->getApplication();
-		$this->config = $this->application->getConfig();
-		$this->controllerName = $this->request->getControllerName();
-		$this->actionName = $this->router->getActionName();
-	}
-
-	public function getResponse()
-	{
-		return $this->response;
-	}
-
-	public function setResponse(Response $response)
-	{
-		$this->response = $response;
 	}
 
 	/**
