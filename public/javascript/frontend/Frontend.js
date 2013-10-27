d0af04710dc9aa3266593f65c0ea6601bf630b52
Rinalds Uzkalns <rinalds@integry.net>
2013-10-28 00:05:30 +0200

    Angular frontend module dependency changes

--- public/javascript/frontend/Frontend.js
+++ public/javascript/frontend/Frontend.js
@@ -2,4 +2,4 @@
  *	@author Integry Systems
  */
 
-var app = angular.module('LiveCart', ['backendComponents', 'ui.bootstrap', 'ui.tinymce', 'ngGrid', 'ngResource', '$strap.directives']);
+var app = angular.module('LiveCart', ['backendComponents', 'ui.bootstrap', 'ngGrid', 'ngResource']);
